3da5197e49e16e3cd168eff043d0d76349eb69d4
khalil <khalillakhdharatc@gmail.com>
2021-12-05 10:27:13 +0100

    finale

--- src/main/java/com/sec/security/SecurityConfig.java
+++ src/main/java/com/sec/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.sec.security;
-
-import javax.activation.DataSource;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -10,31 +9,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
-	private javax.sql.DataSource dataSource;
+	private DataSource dataSource;
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	/*auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("USER","ADMIN");
-	auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER"); */
-	auth.jdbcAuthentication().dataSource(dataSource)
-			.usersByUsernameQuery(
-					"select username as principal ,password, active as credentials from app_user where username=?")
-			.authoritiesByUsernameQuery(
-					"select app_user_username as principal , roles_role_name as role from app_user_roles "
-							+ "where app_user_username=?")
-			.passwordEncoder(bCryptPasswordEncoder).rolePrefix("ROLE_");
+//auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("USER","ADMIN");
+//auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery(
+						"select username as principal ,password, active as credentials from app_user where username=?")
+				.authoritiesByUsernameQuery(
+						"select app_user_username as principal , roles_role_name as role from app_user_roles "
+								+ "where app_user_username=?")
+				.passwordEncoder(bCryptPasswordEncoder).rolePrefix("ROLE_");
 	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	http.formLogin();
-	http.csrf().disable();
-	http.authorizeRequests().antMatchers("/login/**","register/**").permitAll();
-	http.authorizeRequests().antMatchers(HttpMethod.POST,"/tasks/**").hasRole("ADMIN");
-	http.authorizeRequests().anyRequest().authenticated();
+		http.formLogin();
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/login/**", "register/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/tasks/**").hasRole("ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
 	}
 }
